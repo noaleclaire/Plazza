f41b3d94dee10fd672671bb243fd8396ec01daa0
noaleclaire <noa.leclaire@epitech.eu>
2022-05-22 14:41:42 +0200

    MODIF: class Scoped and Mutex in Queue

--- Include/Queue.hpp
+++ Include/Queue.hpp
@@ -23,22 +23,17 @@ class Queue {
 
         void push(T value)
         {
-            std::unique_lock<std::mutex> lock(mutex);
-            // ScopedLock lock(&_mutex, ScopedLock::BASIC);
-            // std::cout << "là1" << std::endl;
+            ScopedLock lock(&_mutex, ScopedLock::BASIC);
             _values.push_back(value);
             _size++;
-            cv.notify_one();
-            // _cv.notifyOne();
+            _cv.notifyOne();
         }
         bool tryPop(T &value)
         {
-            std::unique_lock<std::mutex> lock(mutex, std::defer_lock);
-            // ScopedLock lock(&_mutex, ScopedLock::DEFER);
+            ScopedLock lock(&_mutex, ScopedLock::DEFER);
             while (_values.empty())
                 std::this_thread::sleep_for(std::chrono::seconds(1));
             lock.lock();
-            // lock.lock();
             value = _values.at(0);
             _values.erase(_values.begin());
             _size--;
@@ -46,24 +41,16 @@ class Queue {
         }
         T pop()
         {
-            std::unique_lock<std::mutex> lock(mutex);
-            // ScopedLock lock(&_mutex, ScopedLock::BASIC);
+            ScopedLock lock(&_mutex, ScopedLock::BASIC);
             T value = 0;
 
-            // std::cout << "wsh1" << std::endl;
-            cv.wait(lock, [this]{
+            _cv.wait(lock, [this]{
                 if (_mustQuit)
                     return (true);
                 if (_values.empty())
                     return (false);
                 return (true);
             });
-            // _cv.wait(lock.getLock(), [this]{
-            //     if (_values.empty())
-            //         return (false);
-            //     return (true);
-            // });
-            // std::cout << "wsh2" << std::endl;
             if (!_mustQuit) {
                 value = _values.at(0);
                 _values.erase(_values.begin());
@@ -83,7 +70,7 @@ class Queue {
         void quit()
         {
             _mustQuit = true;
-            cv.notify_all();
+            _cv.notify_all();
         };
 
     protected:
@@ -92,7 +79,5 @@ class Queue {
         ConditionVariable _cv;
         std::vector<T> _values;
         std::size_t _size = 0;
-        std::mutex mutex;
-        std::condition_variable cv;
         bool _mustQuit = false;
 };
